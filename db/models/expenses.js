d0ee9f620f4520bd13858d1f86313b2740d847bb
colleenlavin <cml388@mun.ca>
2017-06-27 14:45:44 -0400

    Adds models

---
+++ db/models/expenses.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const { STRING, INTEGER, DECIMAL, JSON, TEXT } = require('sequelize')
+
+module.exports = db => db.define('expense', {
+
+    childcare: {
+        type: DECIMAL(10, 2),
+        allowNull: false
+    },
+    education: {
+        type: DECIMAL(10, 2),
+        allowNull: false
+    },
+    events: {
+        type: DECIMAL(10, 2),
+        allowNull: false
+    },
+    food: {
+        type: DECIMAL(10, 2),
+        allowNull: false
+    },
+    healthcare: {
+        type: DECIMAL(10, 2),
+        allowNull: false
+    },
+    housing: {
+        type: DECIMAL(10, 2),
+        allowNull: false
+    },
+    insurance: {
+        type: DECIMAL(10, 2),
+        allowNull: false
+    },
+    loans: {
+        type: DECIMAL(10, 2),
+        allowNull: false
+    },
+    transportation: {
+        type: DECIMAL(10, 2),
+        allowNull: false
+    }
+})
+Expense.belongsTo(User)
