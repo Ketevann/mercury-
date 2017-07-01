d659c9fd6bf29e783303e08781fd950bf12a01a3
Kaisie Li <kaisinli@Kaisins-MacBook-Pro.local>
2017-07-01 16:37:12 -0400

    removed hard coded AT

--- server/api.js
+++ server/api.js
@@ -7,7 +7,6 @@ const PLAID_CLIENT_ID = require('../newCredentials').PLAID_CLIENT_ID
 const PLAID_SECRET = require('../newCredentials').PLAID_SECRET
 const PLAID_PUBLIC_KEY = require('../newCredentials').PLAID_PUBLIC_KEY
 const PLAID_ENV = envvar.string('PLAID_ENV', 'sandbox')
-const ACCESS_TOKEN = 'access-sandbox-69aa126f-6075-4325-8afd-fac600c79b5e' // sandbox
 const db = require('../db')
 const AccessToken = db.model('accessToken');
 
@@ -23,13 +22,7 @@ api
   .get('/heartbeat', (req, res) => res.send({ ok: true }))
   .use('/auth', require('./auth'))
   .use('/users', require('./users'))
-  .use('/budget', require('./budget'))
 
-// We store the access_token in memory - in production, store it in a secure
-// persistent data store
-// const ACCESS_TOKEN = null
-
-// Initialize the Plaid client
 const client = new plaid.Client(
   PLAID_CLIENT_ID,
   PLAID_SECRET,
@@ -62,92 +55,106 @@ api.post('/get_access_token', function (request, response, next) {
     console.log('Access Token: ' + ACCESS_TOKEN)
     console.log('Item ID: ' + ITEM_ID)
     response.send(ACCESS_TOKEN)
-    /*response.json({
-      'error': false
-    })*/
   })
 })
 
 api.get('/accounts', function (request, response, next) {
   // Retrieve high-level account information and account and routing numbers
-  // for each account associated with the Item.
-  //let ACCESS_TOKEN = ''
-  console.log('ACCESS', ACCESS_TOKEN)
-  client.getAuth(ACCESS_TOKEN, function (error, authResponse) {
-    console.log('authResponse', authResponse)
-    if (error != null) {
-      let msg = 'Unable to pull accounts from the Plaid API.'
-      // console.log("authResponse", authResponse)
-      console.log(msg + '\n' + error)
-      return response.json({
-        error: msg
-      })
-    }
+  // for each account associated with the Item
 
-    console.log(authResponse.accounts)
-    response.json({
-      error: false,
-      accounts: authResponse.accounts,
-      numbers: authResponse.numbers,
-    })
+  AccessToken.find({
+    where: {
+      user_id: request.user.dataValues.id
+    }
   })
+    .then(foundUser => {
+      client.getAuth(foundUser.dataValues.accessToken, function (error, authResponse) {
+        if (error != null) {
+          let msg = 'Unable to pull accounts from the Plaid API.'
+          // console.log("authResponse", authResponse)
+          console.log(msg + '\n' + error)
+          return response.json({
+            error: msg
+          })
+        }
+
+        console.log(authResponse.accounts)
+        response.json({
+          error: false,
+          accounts: authResponse.accounts,
+          numbers: authResponse.numbers,
+        })
+      })
+    })
 })
 
 api.post('/item', function (request, response, next) {
   // Pull the Item - this includes information about available products,
   // billed products, webhook information, and more.
-  let ACCESS_TOKEN = 'access-sandbox-69aa126f-6075-4325-8afd-fac600c79b5e'
-  client.getItem(ACCESS_TOKEN, function (error, itemResponse) {
-    if (error != null) {
-      console.log(JSON.stringify(error))
-      return response.json({
-        error: error
-      })
+  AccessToken.find({
+    where: {
+      user_id: request.user.dataValues.id
     }
-
-    // Also pull information about the institution
-    client.getInstitutionById(itemResponse.item.institution_id, function (err, instRes) {
-      if (err != null) {
-        let msg = 'Unable to pull institution information from the Plaid API.'
-        console.log(msg + '\n' + error)
-        return response.json({
-          error: msg
-        })
-      } else {
-        response.json({
-          item: itemResponse.item,
-          institution: instRes.institution,
-        })
-      }
-    })
   })
+    .then(foundUser => {
+      client.getItem(foundUser.dataValues.accessToken, function (error, itemResponse) {
+        if (error != null) {
+          console.log(JSON.stringify(error))
+          return response.json({
+            error: error
+          })
+        }
+
+        // Also pull information about the institution
+        client.getInstitutionById(itemResponse.item.institution_id, function (err, instRes) {
+          if (err != null) {
+            let msg = 'Unable to pull institution information from the Plaid API.'
+            console.log(msg + '\n' + error)
+            return response.json({
+              error: msg
+            })
+          } else {
+            response.json({
+              item: itemResponse.item,
+              institution: instRes.institution,
+            })
+          }
+        })
+      })
+    })
 })
 
 api.post('/transactions', function (request, response, next) {
   // Pull transactions for the Item for the last 30 days
-  let ACCESS_TOKEN = 'access-sandbox-69aa126f-6075-4325-8afd-fac600c79b5e'
   // let startDate = moment().subtract(30, 'days').format('YYYY-MM-DD')
-
-  let startDate = '2017-06-01'
-
+  //let startDate = prevMonth 
+  let startDate = '2017-01-01'
   //console.log('DATES', prevMonth, currentMonth)
   // let endDate = moment().format('YYYY-MM-DD')
   //let endDate = currentMonth
-  let endDate = '2017-06-30'
+  let endDate = '2017-02-15'
 
-  client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
-    count: 250,
-    offset: 0,
-  }, function (error, transactionsResponse) {
-    if (error != null) {
-      console.log(JSON.stringify(error))
-      return response.json({
-        error: error
-      })
+  AccessToken.find({
+    where: {
+      user_id: request.user.dataValues.id
     }
-    console.log('pulled ' + transactionsResponse.transactions.length + ' transactions')
-    response.json(transactionsResponse)
   })
+    .then(foundUser => {
+      console.log('USERRR', foundUser)
+      client.getTransactions(foundUser.dataValues.accessToken, startDate, endDate, {
+        count: 250,
+        offset: 0,
+      }, function (error, transactionsResponse) {
+        if (error != null) {
+          console.log(JSON.stringify(error))
+          return response.json({
+            error: error
+          })
+        }
+        console.log('pulled ' + transactionsResponse.transactions.length + ' transactions')
+        response.json(transactionsResponse)
+      })
+    })
 })
 
 api.post('/putTokenInDB', (req, res, next) => {
@@ -156,13 +163,11 @@ api.post('/putTokenInDB', (req, res, next) => {
   var user = req.body.user;
   AccessToken.create(
     {
-    
-        accessToken: req.body.accessToken,
-        user_id: user.id
-      
+      accessToken: req.body.accessToken,
+      user_id: user.id
     })
     .then((aT) => {
-      console.log("AT",aT);
+      console.log("AT", aT);
       res.send(aT);
     })
 })
