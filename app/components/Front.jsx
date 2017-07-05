649067adda1034c77354e1c219de8f74a6893813
colleenlavin <cml388@mun.ca>
2017-07-05 15:16:19 -0400

    Added about to front

--- app/components/Front.jsx
+++ app/components/Front.jsx
@@ -15,16 +15,16 @@ class Front extends Component {
     { console.log(this.props, 'props in Front') }
 
     return (
-    <div className="homepage">
-      <h1>Mercury</h1>
-      <div>
-        <h3>Stop living paycheck to paycheck and control your life.</h3>
-      </div>
-      <button type="button" className="btn btn-secondary"><Link id="about" to="/about">Learn More</Link></button>
-      <button onClick={() => store.dispatch(this.props.modalShow())} type="button" className="btn btn-warning">Sign Up</button>
-      {this.props.modal.signup ? <View /> : null}
-
-    </div>)
+      <div className="homepage">
+        <h2 className="tex header "> >Stop living paycheck to paycheck and control your life!</h2>
+        <h4 className="text ">Mercury helps you see all your accounts at one place</h4>
+        <h4 className="hometext "> understand where your money goes, reduce unwanted spending, and save for future goals</h4>
+        <div className="btn-group" role="group" aria-label="Basic example">
+          <button type="button" className="btn btn-secondary"><Link id="about" to="/about">Learn More</Link></button>
+          <button onClick={() => store.dispatch(this.props.modalShow())} type="button" className="btn btn-warning">Sign Up</button>
+          {this.props.modal.signup ? <View /> : null}
+        </div>
+      </div>)
   }
 }
 
