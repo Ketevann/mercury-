386dbd5d3dceeab044b3e07cc06518dc4de4e7b2
Ketevann <ketevan.tsin@gmail.com>
2017-07-05 18:09:33 -0400

    navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router";
 import Login from './Login'
 import WhoAmI from './WhoAmI'
 import View from './Modal'
-import {connect} from 'react-redux'
-import {modalShow} from "../reducers/modal"
+import { connect } from 'react-redux'
+import { modalShow } from "../reducers/modal"
 import store from '../store'
-import {logout} from 'APP/app/reducers/auth'
-import { connectPlaid} from '../reducers/plaid'
+import { logout } from 'APP/app/reducers/auth'
+import { connectPlaid } from '../reducers/plaid'
 
 
 
@@ -20,43 +20,38 @@ class Navbar extends Component {
     this.handleClick = this.handleClick.bind(this)
   }
   handleClick() {
-      console.log('in click', this.props.modal)
+    console.log('in click', this.props.modal)
     store.dispatch(this.props.modalShow(this.props.modal))
   }
   render() {
-    {console.log(this.props, ' we have props', this.props.user)}
-
+    { console.log(this.props, ' we have props', this.props.user) }
     return (
-        <nav  className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <div className=" wrapper container topnav">
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul role="full-horizontal" className="nav navbar-nav navbar-left">
-                    <li><img className="logo " src={'./logo3.png'} /></li>
-                        <li><Link id="home" to="/home">Home</Link></li>
-
-
-                        {this.props.user  ?
-                        <li><Link id="link" to="/spending">Spending</Link></li> : null}
-                        {this.props.user  ?
-                        <li><Link id="link" to="/budget">Budget</Link></li> : null}
-
-                       {this.props.user  ?
-                        <li><a onClick={this.props.connectPlaid}>Connect to My Account</a></li> : null}
-                        {this.props.user  ?
-                        <li><Link id="link" to="/emailSettings">Email Settings</Link></li> : null}
-
-                    </ul>
-                    <ul className="nav navbar-nav navbar-right">
-                     {!this.props.user ?
-                        <li><Link href="#" onClick={() => this.handleClick()}> Login / Sign Up </Link></li>
-                        :  <div><Link to="" className="logout btn">{this.props.user && this.props.user.name}</Link>
-                         <Link type="button" className="logout btn" onClick={this.props.logout}>Logout</Link></div> }
-                    </ul>
-                </div>
-            </div>
-            {console.log(this.props.modal.showModal, ' in navbar')}
-            {this.props.modal.showModal ? <View /> : null }
-        </nav>
+      <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div className=" wrapper container topnav">
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul role="full-horizontal" className="nav navbar-nav navbar-left">
+              <li><img className="logo " src={'./logo3.png'} /></li>
+              <li><Link id="home" to="/home">Home</Link></li>
+              {this.props.user ?
+                <li><Link id="link" to="/spending">Spending</Link></li> : null}
+              {this.props.user ?
+                <li><Link id="link" to="/budget">Budget</Link></li> : null}
+              {this.props.user ?
+                <li><a onClick={this.props.connectPlaid}>Connect to My Account</a></li> : null}
+              {this.props.user ?
+                <li><Link id="link" to="/emailSettings">Email Settings</Link></li> : null}
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              {!this.props.user ?
+                <li><Link href="#" onClick={() => this.handleClick()}> Login / Sign Up </Link></li>
+                : <div><Link to="" className="logout btn">{this.props.user && this.props.user.name}</Link>
+                  <Link type="button" className="logout btn" onClick={this.props.logout}>Logout</Link></div>}
+            </ul>
+          </div>
+        </div>
+        {console.log(this.props.modal.showModal, ' in navbar')}
+        {this.props.modal.showModal ? <View /> : null}
+      </nav>
 
     )
   }
@@ -68,7 +63,7 @@ class Navbar extends Component {
 
 
 export default connect(
-   ({ modal, auth }) => ({ modal: modal, user: auth }),
-  {modalShow, logout, connectPlaid},
+  ({ modal, auth }) => ({ modal: modal, user: auth }),
+  { modalShow, logout, connectPlaid },
 )(Navbar)
 
