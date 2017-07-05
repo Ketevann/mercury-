2239fe3192b8dc7f5223973fa9949969ef165535
colleenlavin <cml388@mun.ca>
2017-07-05 12:17:14 -0400

    Hardcoded goals page

--- app/components/Goals.jsx
+++ app/components/Goals.jsx
@@ -8,92 +8,75 @@ import store from '../store'
 
 
 export default class Goals extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = store.getState()
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleClick = this.handleClick.bind(this)
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = store.getState()
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  //   this.handleClick = this.handleClick.bind(this)
+  // }
 
 
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState());
-    });
-  }
+  // componentDidMount() {
+  //   this.unsubscribe = store.subscribe(() => {
+  //     this.setState(store.getState());
+  //   });
+  // }
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
+  // componentWillUnmount() {
+  //   this.unsubscribe();
+  // }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+  // handleChange(event) {
+  //   this.setState({ value: event.target.value });
+  // }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    var newDescription = event.target.description.value;
-    axios.post(`/api/goals/newgoal`, {  description: newDescription })
-      .then(res => {
-        console.log(res.data)
-        this.state.goals.push(res.data);
-        this.setState({ goals: this.state.goals })
-        var element1 = document.querySelector('#description');
-        element1.value = '';
-      })
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   var newDescription = event.target.description.value;
+  //   axios.post(`/api/goals/newgoal`, {  description: newDescription })
+  //     .then(res => {
+  //       console.log(res.data)
+  //       this.state.goals.push(res.data);
+  //       this.setState({ goals: this.state.goals })
+  //       var element1 = document.querySelector('#description');
+  //       element1.value = '';
+  //     })
 
-  }
+  // }
 
-  handleClick(event) {
-    const goalId = event.target.value
-    axios.delete(`/api/goals/${goalId}`)
-      .then(res => {
-        const goals = this.state.goals.filter(goal => {
-          return goal.id != goalId
-        })
-        this.setState({ goals: goals })
-      })
-  }
+  // handleClick(event) {
+  //   const goalId = event.target.value
+  //   axios.delete(`/api/goals/${goalId}`)
+  //     .then(res => {
+  //       const goals = this.state.goals.filter(goal => {
+  //         return goal.id != goalId
+  //       })
+  //       this.setState({ goals: goals })
+  //     })
+  // }
 
   render() {
     return (
-           <div className='goalsMain'>
-                <div>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th>ID</th>
-                                <th>DESCRIPTION</th>
-                                <th>IMPORTANCE</th>
-                                <th>DELETE</th>
-                            </tr>
-                            {this.state.goals.map(goal =>
-                                <tr key={goal.id}>
-                                    <td>{goal.id}</td>
-                                    <td>
-                                        <Link to={`/goals/${goal.id}`}>
-                                            {goal.description}
-                                        </Link>
-                                    </td>
-                                    <td>{goal.importance}</td>
-                                    <td><button value={goal.id} onClick={this.handleClick}>
-                                        X
-                                        </button>
-                                    </td>
-                                </tr>)}
-                        </tbody>
-                    </table>
-                </div>
-                {/*Temporary form. Make form its own component/container combo.*/}
-                <form id="form" onSubmit={this.handleSubmit}>
-                    <div className="text"> Add New goal </div>
-                    <div>
-                    <label> Description: </label>
-                    <input name='description' id='description' onChange={this.handleChange} placeholder="Description"/>
-                    </div>
-                    <button>Submit</button>
-                </form>
+      <div className='goalsMain'>
+        <div id="myDIV" class="header">
+          <h2>My Goals</h2>
+        </div>    
+          <ul id="myUL">
+            <li>Save 20% of my income</li>
+            <li className="checked">Pay bills</li>
+            <li>Learn what a 401k is</li>
+            <li>Pay off my student loans</li>
+            <li>Start a vacation fund</li>
+          </ul>
+          <form id="form" onSubmit={this.handleSubmit}>
+            <div className="text"> Add New goal </div>
+            <div>
+              <label> Description: </label>
+              <input name='description' id='description' onChange={this.handleChange} placeholder="Description" />
             </div>
-    );
+            <button>Submit</button>
+          </form>
+        </div>
+        );
   }
 }
