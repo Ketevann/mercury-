31fa74922ef8ba444d994b4ef96dde5030c46fbf
Ketevann <ketevan.tsin@gmail.com>
2017-07-19 22:05:04 -0400

    fixed login, need to work on modal form

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -20,126 +20,124 @@ import { menuShow, menuHide } from "../reducers/dropdown"
 class Navbar extends Component {
   constructor(props) {
     super(props)
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClicker = this.handleClick.bind(this)
     this.clickHandler = this.clickHandler.bind(this)
 
   }
-  handleClick() {
-    store.dispatch(this.props.modalShow(this.props.modal))
+
+  handleClicker() {
+    console.log("clickinnn")
+    store.dispatch(this.props.modalShow())
   }
   handleClick = () => this.props.menuShow()
   handleClose = () => this.props.menuHide()
 
-  clickHandler(){
+  clickHandler() {
     this.props.logout()
     browserHistory.push('/')
-var disp
-if (this.props.menu.menuShow) disp = "block"
-else disp = 'none'
-
-
 
   }
 
   render() {
- var disp
-if (this.props.menu.showMenu === true) disp = 'block'
-else disp = 'none'
+    var disp
+    if (this.props.menu.showMenu === true) disp = 'block'
+    else disp = 'none'
     const divStyle = {
       display: disp, // note the capital 'W' here
-   // 'ms' is the only lowercase vendor prefix
-};
-var opp
-if (store.getState().browser.is.small ===true || store.getState().browser.is.extraSmall===true ) opp = 'none'
-else opp = 'block'
-const divStyle2 = {
+      // 'ms' is the only lowercase vendor prefix
+    };
+    var opp
+    if (store.getState().browser.is.small === true || store.getState().browser.is.extraSmall === true) opp = 'none'
+    else opp = 'block'
+    const divStyle2 = {
       display: opp, // note the capital 'W' here
-   // 'ms' is the only lowercase vendor prefix
-};
-    {console.log(this.props, 'props in modal')}
+      // 'ms' is the only lowercase vendor prefix
+    };
+    { console.log(this.props, 'props in modal') }
     return (
       <div>
 
 
-      <div>
+        <div>
 
-        <div className="navbar-more-overlay" />
-        <nav className="navbar navbar-inverse navbar-fixed-top animate">
-          <div className="container navbar-more visible-xs">
+          <div className="navbar-more-overlay" />
+          <nav className="navbar navbar-inverse navbar-fixed-top animate">
+            <div className="container navbar-more visible-xs">
 
-
-          </div>
-          <div className="container">
-            <div className="navbar-header hidden-xs">
-                          <li><img className="logo menu-icon" src={'./logo3.png'} /></li>
 
             </div>
-            {console.log($(window).width(), store.getState(),' meeed',store.getState().browser.is.medium, opp, 'oppsss')}
-             {store.getState().browser.is.medium ===true ?
-             console.log("yay")
- : console.log("nooooo")}
- {store.getState().browser.is.small ===true || store.getState().browser.is.extraSmall===true  ?
-  <div className="mobbar">
-            <i   className="fa fa-bars fa-2x" onClick={() => this.handleClick()} />
-            <ul  style={divStyle} className="nav navbar-nav navbar-right mobile-bar " >
+            <div className="container">
+              <div className="navbar-header hidden-xs">
+                <li><img className="logo menu-icon" src={'./logo3.png'} /></li>
 
-                        <li><Link className="menu-icon" id="home" to="/home">Home</Link></li>
-              {this.props.user ?
-                <li><Link className="menu-icon" id="link" to="/budget">Budget</Link></li> : null}
+              </div>
+              {console.log($(window).width(), store.getState(), ' meeed', store.getState().browser.is.medium, opp, 'oppsss')}
+              {store.getState().browser.is.medium === true ?
+                console.log("yay")
+                : console.log("nooooo")}
+              {store.getState().browser.is.small === true || store.getState().browser.is.extraSmall === true ?
+                <div className="mobbar">
+                  <i className="fa fa-bars fa-2x" onClick={() => this.handleClick()} />
+                  <ul style={divStyle} className="nav navbar-nav navbar-right mobile-bar " >
+
+                    <li><Link className="menu-icon" id="home" to="/home">Home</Link></li>
+                    {this.props.user ?
+                      <li><Link className="menu-icon" id="link" to="/budget">Budget</Link></li> : null}
+                    {this.props.user ?
+                      <li><a className="menu-icon" onClick={this.props.connectPlaid}>Connect to My Account</a></li> : null}
+                    {this.props.user ?
+                      <li><Link className="menu-icon" id="link" to="/emailSettings">Email Settings</Link></li> : null}
+                    {!this.props.user ?
+                      <li><Link className="menu-icon" href="#" onClick={() =>store.dispatch(this.props.modalShow())}> Login / Sign Up </Link></li>
+                      : null}
+                    {this.props.user ?
+                      <li className="menu-icon"><Link className="menu-icon" to="" id="name" >{this.props.user && this.props.user.name}</Link></li> : null}
+                    {this.props.user ?
+                      <li>
+                        <Link className="menu-icon" type="button" id="logbtn" onClick={() => this.clickHandler()}>Logout</Link></li> : null}
+
+                  </ul>
+                </div>
+                : null}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              <ul style={divStyle2} className="nav navbar-nav navbar-right mobile-bar">
+
+                <li><Link className="menu-icon" id="home" to="/home">Home</Link></li>
                 {this.props.user ?
-                <li><a className="menu-icon" onClick={this.props.connectPlaid}>Connect to My Account</a></li> : null}
-              {this.props.user ?
-                <li><Link className="menu-icon" id="link" to="/emailSettings">Email Settings</Link></li> : null}
+                  <li><Link className="menu-icon" id="link" to="/budget">Budget</Link></li> : null}
+                {this.props.user ?
+                  <li><a className="menu-icon" onClick={this.props.connectPlaid}>Connect to My Account</a></li> : null}
+                {this.props.user ?
+                  <li><Link className="menu-icon" id="link" to="/emailSettings">Email Settings</Link></li> : null}
                 {!this.props.user ?
-                <li><Link className="menu-icon" href="#" onClick={() => this.handleClick()}> Login / Sign Up </Link></li>
-                : null }
-                 {this.props.user ?
-                 <li className="menu-icon"><Link className="menu-icon" to="" id="name" >{this.props.user && this.props.user.name}</Link></li> : null}
-                  {this.props.user ?
-                    <li>
-                  <Link className="menu-icon" type="button" id="logbtn"  onClick={()=> this.clickHandler()}>Logout</Link></li> : null}
+                  <li><Link className="menu-icon" href="#" onClick={() => store.dispatch(this.props.modalShow())}> Login / Sign Up </Link></li>
+                  : null}
+                {this.props.user ?
+                  <li className="menu-icon"><Link className="menu-icon" to="" id="name" >{this.props.user && this.props.user.name}</Link></li> : null}
+                {this.props.user ?
+                  <li>
+                    <Link className="menu-icon" type="button" id="logbtn" onClick={() => this.clickHandler()}>Logout</Link></li> : null}
 
-            </ul>
+              </ul>
             </div>
-            : null}
+          </nav>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-            <ul style={divStyle2} className="nav navbar-nav navbar-right mobile-bar">
-
-                        <li><Link className="menu-icon" id="home" to="/home">Home</Link></li>
-              {this.props.user ?
-                <li><Link className="menu-icon" id="link" to="/budget">Budget</Link></li> : null}
-                {this.props.user ?
-                <li><a className="menu-icon" onClick={this.props.connectPlaid}>Connect to My Account</a></li> : null}
-              {this.props.user ?
-                <li><Link className="menu-icon" id="link" to="/emailSettings">Email Settings</Link></li> : null}
-                {!this.props.user ?
-                <li><Link className="menu-icon" href="#" onClick={() => this.handleClick()}> Login / Sign Up </Link></li>
-                : null }
-                 {this.props.user ?
-                 <li className="menu-icon"><Link className="menu-icon" to="" id="name" >{this.props.user && this.props.user.name}</Link></li> : null}
-                  {this.props.user ?
-                    <li>
-                  <Link className="menu-icon" type="button" id="logbtn"  onClick={()=> this.clickHandler()}>Logout</Link></li> : null}
-
-            </ul>
-          </div>
-        </nav>
-                {this.props.modal.showModal ? <View /> : null}
-
-      </div>
+        </div>
+        {this.props.modal.showModal ? <View /> : null}
       </div>
     )
   }
