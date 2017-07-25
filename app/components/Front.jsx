284d3236b4b33a50ad88eb5176ae4d2162c9adb9
Ketevann <ketevan.tsin@gmail.com>
2017-07-24 23:16:20 -0400

    Update Front.jsx

--- app/components/Front.jsx
+++ app/components/Front.jsx
@@ -2,8 +2,8 @@ import {connect} from 'react-redux'
 import React, { Component } from 'react'
 import { Link } from "react-router";
 import store from '../store'
-import {modalShow, modalHide, Login, Signup, forgot, newPassowrd} from '../reducers/modal'
-import View from './Modal'
+import {modalShow} from '../reducers/modal'
+import Modal from './Modal'
 
 
 
@@ -17,7 +17,7 @@ class Front extends Component {
       <h4  className="text ">Mercury helps you keep track of your budget and spending</h4>
       <h4 className="hometext "> and alerts you when you go off-track</h4>
       <button onClick={() => store.dispatch(this.props.modalShow()) } type="button" className="btn btn-warning signup">Sign Up</button>
-      {this.props.modal.signup ? <View /> :null}
+      {this.props.modal.signup ? <Modal /> :null}
 
 </div>)
   }
@@ -26,6 +26,6 @@ class Front extends Component {
 
 export default connect(
    ({ modal, auth }) => ({ modal: modal, user: auth }),
-  {modalShow, modalHide, Login, Signup, forgot, newPassowrd},
+  {modalShow},
 )(Front)
 
