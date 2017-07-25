4c498dd17401c806df3290d58c958d44c1dc3884
Ketevann <ketevan.tsin@gmail.com>
2017-07-24 23:01:15 -0400

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -30,27 +30,26 @@ class Navbar extends Component {
 
   clickHandler() {
     this.props.logout()
-    browserHistory.push('/') {/*redirect to homepage after logout */}
+    browserHistory.push('/')
 
   }
 
   render() {
     var disp, displayStyle
-     {/*toggle for smaller screens; if showMenu is true, display the navbar under the menu icon. If false hide it */}
+
     if (this.props.menu.showMenu === true)
       disp = 'inline-block'
     else disp = 'none'
     const divStyle = {
       display: disp
     };
-    {/*if the screen size is small or extra small display the menu icon, otherwise display none*/}
+
     if (store.getState().browser.is.small === true || store.getState().browser.is.extraSmall === true) displayStyle = 'inline'
     else displayStyle = 'none'
 
     const iconStyle = {
       display: displayStyle
     };
-    {/*always display the navbar menu for larger screen size*/}
     if (store.getState().browser.is.small !== true && store.getState().browser.is.extraSmall !== true) divStyle.display = 'block'
 
     return (
@@ -66,8 +65,9 @@ class Navbar extends Component {
               <ul className="menulist nav navbar-nav" style={divStyle}>
                 <li><Link className="menuicon" id="home" to="/home">Home</Link></li>
                 <li><a className="menuicon" onClick={this.props.connectPlaid}>Connect to My Account</a></li>
-                <li><Link className="menuicon" id="link" to="/emailSettings">Email Settings</Link></li>     <li className="menuicon"><Link className="menuicon" to="" id="name" >{this.props.user && this.props.user.name}</Link></li>
-                <li><Link className="menuicon" id="link" to="/budget">Budget</Link></li>
+                <li><Link className="menuicon" id="link" to="/emailSettings">Email Settings</Link></li>
+                 <li><Link className="menuicon" id="link" to="/budget">Budget</Link></li>
+                <li className="menuicon"><Link className="menuicon" to="" id="name" >{this.props.user && this.props.user.name}</Link></li>
                 <li><Link className="menuicon" type="button" id="logbtn" onClick={() => this.clickHandler()}>Logout</Link></li>
               </ul>
               : <ul className="menulist nav navbar-nav" style={divStyle}><li><Link className="menuicon" href="#" onClick={() => store.dispatch(this.props.modalShow())}> Login / Sign Up </Link></li></ul>}
