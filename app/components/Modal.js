9bfedfa3265ed1c634c68064c5bdc057d7eace89
Ketevann <ketevan.tsin@gmail.com>
2017-07-24 22:59:21 -0400

    Update Modal.js

--- app/components/Modal.js
+++ app/components/Modal.js
@@ -1,28 +1,31 @@
 import React, { PropTypes } from 'react';
 import { ModalContainer, ModalDialog } from 'react-modal-dialog';
-import { modalShow, modalHide, Login, Signup, forgot, newPassowrd } from '../reducers/modal'
+import { showModal, hideModal } from '../reducers/modal'
+import { boolLogin, boolSignUp, boolPassword } from '../reducers/login'
+
 import { connect } from 'react-redux'
 import { login, signup } from 'APP/app/reducers/auth'
-
-
+import LoginForm from './LoginForm'
+import PasswordForm from './PasswordForm'
+import SignupForm from './SignupForm'
 import store from '../store'
 
 
-class View extends React.Component {
+class Modal extends React.Component {
 
   constructor() {
     super()
     this.handleSubmit = this.handleSubmit.bind(this)
   }
-  handleClick = () => store.dispatch(modalShow())
-  handleClose = () => store.dispatch(modalHide())
+  handleClick = () => this.props.showModal()
+  handleClose = () => this.props.hideModal()
 
   handleSubmit(evt) {
     evt.preventDefault()
-    let credentials = { email: evt.target.email.value, password: evt.target.password.value }
-    this.props.newPassowrd(credentials)
+    this.props.newPassowrd({ email: evt.target.email.value, password: evt.target.password.value })
     this.handleClose()
   }
+
   render() {
     return (
       <div className="modal" onClick={this.handleClick}>{
@@ -32,54 +35,38 @@ class View extends React.Component {
               <div className="clear"></div>
               <br></br>
               <br></br>
-              {this.props.modal.login === true ?
+              {this.props.status.login === true ?
                 <div>
-                  <h5> <a onClick={() => store.dispatch(this.props.Signup())} className="alignright">Sign up</a></h5>
+                  <h5> <a onClick={() => this.props.boolSignUp()} className="alignright">Sign up</a></h5>
                   <span className="alignright" >Not a member? </span>
                   <br></br>
                   <h2 clssName="clear">Member Login</h2>
-                  <form onSubmit={evt => {
-                    evt.preventDefault()
-                    this.props.login(evt.target.email.value, evt.target.password.value)
-                    this.handleClose()
-                  }
-                  }>
-                    <input className="credentials" name="email" placeholder="Email" required />
-                    <br></br>
-                    <input className="credentials" name="password" type="password" placeholder="Password" required />
-                    <br></br>
-                    <br></br>
-                    <input className="btn" type="submit" value="Login" />
-                  </form>
-                  <h5> <a href="#" onClick={() => this.props.forgot()}>Forgot Password?</a></h5>
-                  {this.props.modal.forgotPassword ?
-                    <form onSubmit={(evt) => this.handleSubmit(evt)}>
-                      <input name="email" placeholder="Email" required />
-                      <input name="password" type="password" placeholder="New Password" required />
-                      <input className="btn" type="submit" value="Change Password" />
-                    </form> : null}
+                  <LoginForm />
+                  <PasswordForm />
                 </div> :
                 <div>
-                  <h5> <a href="#" onClick={() => this.props.Login()} className="alignright">Login</a></h5>
+                  <h5> <a href="#" onClick={() => this.props.boolLogin()} className="alignright">Login</a></h5>
                   <span className="alignright" >Already a member? </span>
                   <br></br>
                   <br></br>
                   <h2 clssName="clear">Join Mercury</h2>
+                  <SignupForm />
+                </div> : null}
                   <form onSubmit={evt => {
-                    evt.preventDefault()
-                    this.props.signup(evt.target.email.value, evt.target.password.value, evt.target.name.value)
-                  }
-                  }>
-                    <input className="credentials" name="name" placeholder="Name" required />
-                    <br></br>
-                    <input className="credentials" name="email" placeholder="Email" required />
-                    <br></br>
-                    <input className="credentials" name="password" type="password" placeholder="Password" required />
-                    <br></br>
-                     <br></br>
+                evt.preventDefault()
+                this.props.signup(evt.target.email.value, evt.target.password.value, evt.target.name.value)
+              }
+              }>
+                <input className="credentials" name="name" placeholder="Name" required />
+                <br></br>
+                <input className="credentials" name="email" placeholder="Email" required />
+                <br></br>
+                <input className="credentials" name="password" type="password" placeholder="Password" required />
+                <br></br>
+                <br></br>
 
-                    <input className="btn" type="submit" value="Sign Up" />
-                  </form></div>}
+                <input className="btn" type="submit" value="Sign Up" />
+              </form>
               <br></br>
               <a href="/api/auth/login/google"> <button className="google"></button> </a>
             </ModalDialog>
@@ -91,6 +78,6 @@ class View extends React.Component {
 
 
 export default connect(
-  ({ modal }) => ({ modal: modal }),
-  { modalShow, modalHide, login, signup, Login, Signup, forgot, newPassowrd },
-)(View)
+  ({ modal, status }) => ({ modal: modal, status: status }),
+  { showModal, hideModal, boolLogin, boolSignUp },
+)(Modal)
